76d5f4018d19f706bba6dd86a1ea4492b826a9f7
precto1285 <prexwebs@gmail.com>
2024-05-12 20:19:48 -0700

    python quick play

---
+++ pythonProject2024.py
@@ -0,0 +1,7 @@
+x = input("enter a number: ")
+y = input("enter a second number: ")
+
+if x < y:
+    print("x equals {}. y equals {}. Therefore, x is lesser than y.".format(x,y))
+else:
+    print ("x equals {}. y equals {}. Therefore, x is greater than y.".format(x,y))
